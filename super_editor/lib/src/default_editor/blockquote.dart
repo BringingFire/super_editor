fe46f3319e3c4807233cc052f48791af3e107f84
Joshua Matthews <josh@jmatth.com>
2022-02-04 17:08:23 -0500

    Replace remaining instances of custom logger with the logging package

--- super_editor/lib/src/default_editor/blockquote.dart
+++ super_editor/lib/src/default_editor/blockquote.dart
@@ -4,7 +4,6 @@ import 'package:flutter/services.dart';
 import 'package:super_editor/src/core/edit_context.dart';
 import 'package:super_editor/src/core/editor.dart';
 import 'package:super_editor/src/default_editor/attributions.dart';
-import 'package:super_editor/src/infrastructure/_logging.dart';
 import 'package:super_editor/src/infrastructure/attributed_text_styles.dart';
 import 'package:super_editor/src/infrastructure/keyboard.dart';
 
@@ -13,9 +12,6 @@ import 'layout_single_column/layout_single_column.dart';
 import 'paragraph.dart';
 import 'text.dart';
 import 'text_tools.dart';
-
-// ignore: unused_element
-final _log = Logger(scope: 'blockquote.dart');
 
 class BlockquoteComponentBuilder implements ComponentBuilder {
   const BlockquoteComponentBuilder();
