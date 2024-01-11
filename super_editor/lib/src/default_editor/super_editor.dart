9b3a387887cc4a72e8e7bd743fa04f1bd10c114e
Joshua Matthews <josh@jmatth.com>
2024-01-11 17:00:54 -0500

    Parameterizing document layout

--- super_editor/lib/src/default_editor/super_editor.dart
+++ super_editor/lib/src/default_editor/super_editor.dart
@@ -134,6 +134,7 @@ class SuperEditor extends StatefulWidget {
     this.plugins = const {},
     this.scrollOff = AxisOffset.zero,
     this.debugPaint = const DebugPaintConfig(),
+    this.documentLayoutBuilder,
   })  : stylesheet = stylesheet ?? defaultStylesheet,
         selectionStyles = selectionStyle ?? defaultSelectionStyle,
         componentBuilders = componentBuilders != null
@@ -329,6 +330,9 @@ class SuperEditor extends StatefulWidget {
   /// Paints some extra visual ornamentation to help with
   /// debugging.
   final DebugPaintConfig debugPaint;
+
+  /// The [DocumentLayoutBuilder] to pass to [DocumentScaffold].
+  final DocumentLayoutBuilder? documentLayoutBuilder;
 
   @override
   SuperEditorState createState() => SuperEditorState();
@@ -618,6 +622,7 @@ class SuperEditorState extends State<SuperEditor> {
                   autoScrollController: _autoScrollController,
                   scroller: _scroller,
                   presenter: presenter,
+                  documentLayoutBuilder: widget.documentLayoutBuilder,
                   componentBuilders: widget.componentBuilders,
                   underlays: [
                     // Add all underlays that the app wants.
